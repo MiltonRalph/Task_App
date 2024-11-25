76b386c2646d392b37e30b0e2535ed9ef6acec17
HAMILTON JUDE <hamiltonjude679@gmail.com>
2024-11-25 13:26:07 -0200

    Updated security

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const taskRoutes = require('./routes/taskRoutes');
 const userRoutes = require('./routes/userRoutes');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(helmet());
 
 app.use('/api/v1/tasks', taskRoutes);
 app.use('/api/v1/users', userRoutes);
